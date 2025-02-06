be01152506a12d20dd68a6ef0e79e5985cf6df57
jotave <skai7ami@gmail.com>
2025-02-06 13:02:38 -0300

    add Database

--- HappyDesk.Domain/Contants/Filenames.cs
+++ HappyDesk.Domain/Contants/Filenames.cs
@@ -3,4 +3,5 @@ namespace HappyDesk.Domain.Contants;
 public static class Filenames
 {
     public const string Icon = "pack://application:,,,/Resources/icon.ico";
+    public const string Database = "database.sqlite";
 }
