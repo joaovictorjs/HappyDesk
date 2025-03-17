16fe61d40db43cffb9ad420f365eb96277008f52
jotave <skai7ami@gmail.com>
2025-03-17 11:44:12 -0300

    change shutdown mode, handle exception and resolde Tray view model

--- HappyDesk.Presentation/App.xaml.cs
+++ HappyDesk.Presentation/App.xaml.cs
@@ -1,15 +1,40 @@
-﻿using HappyDesk.Presentation.ViewModels;
+﻿using System.Windows;
+using System.Windows.Threading;
+using HappyDesk.Domain.Constants;
+using HappyDesk.Domain.Interfaces;
+using HappyDesk.Presentation.DependencyInjection;
+using HappyDesk.Presentation.ViewModels;
 using Application = System.Windows.Application;
 
 namespace HappyDesk.Presentation;
 
 /// <summary>
-/// Interaction logic for App.xaml
+///     Interaction logic for App.xaml
 /// </summary>
 public partial class App : Application
 {
+    private readonly DependencyInjector _injector;
+
     public App()
     {
-        var trayVm = new TrayViewModel();
+        _injector = DependencyInjector.Instance;
+        DispatcherUnhandledException += HandleUnhandledExceptions;
+        ViewModelLocationProvider.SetDefaultViewModelFactory(_injector.Resolve);
+        ShutdownMode = ShutdownMode.OnExplicitShutdown;
+    }
+
+    private void HandleUnhandledExceptions(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        var message = e.Exception.Message;
+        message += e.Exception.InnerException?.Message.Insert(0, "\n\n");
+        _injector.Resolve<IMessageBoxService>().ShowError(message);
+        _injector.Resolve<ILoggerService>().Log(message);
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        _injector.Resolve<TrayViewModel>();
     }
 }
