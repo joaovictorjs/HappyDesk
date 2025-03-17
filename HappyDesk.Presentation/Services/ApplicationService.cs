00d460f629e2a1ea9b48d6dec77015dd4e169034
jotave <skai7ami@gmail.com>
2025-03-17 11:45:39 -0300

    implement shutdown

--- HappyDesk.Presentation/Services/ApplicationService.cs
+++ HappyDesk.Presentation/Services/ApplicationService.cs
@@ -1,13 +1,17 @@
-using System.IO;
 using HappyDesk.Domain.Interfaces;
 using Application = System.Windows.Application;
 
 namespace HappyDesk.Presentation.Services;
 
-public class ApplicationService:IApplicationService
+public class ApplicationService : IApplicationService
 {
     public object GetIcon(string path)
     {
         return new Icon(Application.GetResourceStream(new Uri(path))!.Stream);
+    }
+
+    public void Shutdown()
+    {
+        Application.Current.Shutdown();
     }
 }
