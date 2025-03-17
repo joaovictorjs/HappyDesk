a55f9e739d0d4b90871544b64e7e1972aff31f93
jotave <skai7ami@gmail.com>
2025-03-17 11:46:31 -0300

    .

--- HappyDesk.Domain/Contants/Filenames.cs
+++
@@ -1,7 +0,0 @@
-namespace HappyDesk.Domain.Contants;
-
-public static class Filenames
-{
-    public const string Icon = "pack://application:,,,/Resources/icon.ico";
-    public const string Database = "database.sqlite";
-}
